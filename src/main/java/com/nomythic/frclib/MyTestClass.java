759ce77de3d2968f34ae035ec55c3040f8844f60
Chris Ward <chris.ward@improving.com>
2026-08-03 09:31:29 -0500

    not sure

--- src/main/java/com/nomythic/frclib/MyTestClass.java
+++ src/main/java/com/nomythic/frclib/MyTestClass.java
@@ -1,11 +1,26 @@
 package com.nomythic.frclib;
 
-public class MyTestClass {
-  public boolean isThisWorking() {
-    return true;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.*;
+
+public class MyTestClass extends SubsystemBase {
+
+  private XboxController myController;
+
+  public MyTestClass() {
+    this.myController = new XboxController(0);
   }
 
+  public boolean isThisWorking() {
+    return this.myController.getAButton();
+  }
+
+  public void getStuff() {
+  
+  }
   public void sayHello() {
     System.out.println("Hello");
   }
+
+
 }
